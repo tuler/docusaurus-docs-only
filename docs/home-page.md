479ce5479d1f651a205f17e5d8ab9167b22801aa
Danilo Tuler <danilo@cartesi.io>
2020-06-23 10:49:07 -0400

    Turning docs-only mode on

---
+++ docs/home-page.md
@@ -0,0 +1,7 @@
+---
+id: home-page
+title: Home Page
+sidebar_label: Home Page
+---
+
+Home page test
--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,6 @@
 module.exports = {
   someSidebar: {
+    'Getting started': ['home-page'],
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
   },
